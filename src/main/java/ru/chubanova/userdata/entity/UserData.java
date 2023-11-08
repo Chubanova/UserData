130b84a1d8b6b5556de3afdd723d6dfa1361363f
Chubanova <m.esaeva@inbox.ru>
2023-11-08 17:14:00 +0300

    add userCon

--- src/main/java/ru/chubanova/userdata/entity/UserData.java
+++ src/main/java/ru/chubanova/userdata/entity/UserData.java
@@ -1,4 +1,17 @@
 package ru.chubanova.userdata.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class UserData {
+    @Id
+    private Integer id;
+    private String name;
+    private String docNum;
+    private String phone;
+
 }
--- src/main/java/ru/chubanova/userdata/primaryadapter/UserController.java
+++ src/main/java/ru/chubanova/userdata/primaryadapter/UserController.java
@@ -1,23 +1,24 @@
 package ru.chubanova.userdata.primaryadapter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.chubanova.userdata.application.EventService;
 import ru.chubanova.userdata.entity.UserData;
 
 @RestController
 @RequestMapping("user-rest")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final EventService eventService;
 
     @PostMapping(value = "/userDataEvent")
     @ResponseStatus(value = HttpStatus.OK)
-    public void event(UserData userData){
+    public void event( @RequestBody UserData userData){
+        log.info("Получен пользователь ="+userData.toString());
+
         eventService.saveUserData(userData);
 
     }
